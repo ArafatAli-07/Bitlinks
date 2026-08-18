fdd1dc94795a86cb1653f6af0a0eaf1548e68ff0
Arafat Ali <aliarafat801111@gmail.com>
2026-08-18 18:02:40 +0530

    Improve POST request error handling for short URLs
    Refactor error handling and response structure in POST request.

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,24 +3,56 @@ import User from "@/models/User";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  try{
+  try {
     await connectionToDatabase();
-    const {url, shorturl} = await request.json()
+
+    const { url, shorturl } = await request.json();
+
+    if (!url || !shorturl) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "URL and short URL are required.",
+        },
+        { status: 400 }
+      );
+    }
 
     const existingUser = await User.findOne({ shorturl });
 
     if (existingUser) {
       return NextResponse.json(
-        { success: false, message: "Short URL already exists." },
-        { status: 409 } // 409 Conflict
+        {
+          success: false,
+          message: "Short URL already exists.",
+        },
+        { status: 409 }
       );
     }
 
-    const newUser = new User({url, shorturl});
+    const newUser = new User({
+      url,
+      shorturl,
+    });
+
     await newUser.save();
-    return NextResponse.json({ success: true, message: "Short URL generated successfully" },
-      { status: 200 });
-  }catch(err){
-    console.log(err);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Short URL generated successfully",
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Error creating short URL:", err);
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
   }
 }
